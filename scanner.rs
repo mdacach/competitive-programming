7c8f586e305bb05ff56de02e74538007d181f8bb
Matheus Cardoso <dacachmatheus@gmail.com>
2023-05-20 16:21:20 -0300

    Add scanner template

---
+++ scanner.rs
@@ -0,0 +1,25 @@
+struct Scanner {
+    tokens: std::collections::VecDeque<String>,
+}
+
+impl Scanner {
+    pub fn new() -> Self {
+        use std::io::BufRead;
+
+        let stdin = std::io::stdin();
+        let mut tokens = std::collections::VecDeque::new();
+        for line in stdin.lock().lines() {
+            for token in line.unwrap().split_ascii_whitespace() {
+                tokens.push_back(token.to_owned());
+            }
+        }
+        Self { tokens }
+    }
+
+    pub fn next<T: std::str::FromStr>(&mut self) -> T
+        where
+            <T as std::str::FromStr>::Err: std::fmt::Debug,
+    {
+        T::from_str(&self.tokens.pop_front().unwrap()).unwrap()
+    }
+}
