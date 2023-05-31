4e1117c2cba4959447b11f0cb96ff5320c564bea
Matheus Cardoso <dacachmatheus@gmail.com>
2023-05-31 18:50:20 -0300

    Add solution to 269B [1700]

---
+++ contest/269/b/src/main.rs
@@ -0,0 +1,111 @@
+use std::cmp::{max, min};
+
+fn main() {
+    let mut scanner = Scanner::new();
+
+    let (n, m): (usize, usize) = (scanner.next(), scanner.next());
+    let plants: Vec<_> = (0..n)
+        .map(|_| {
+            let species: u16 = scanner.next();
+            let _position: f32 = scanner.next();
+            species
+        })
+        .collect();
+
+    let mut least_end_value_for_length = vec![u16::MAX; n + 1];
+    least_end_value_for_length[0] = 0;
+
+    for v in plants {
+        let pos = least_end_value_for_length.partition_point(|&x| x <= v);
+        if least_end_value_for_length[pos] > v {
+            least_end_value_for_length[pos] = v;
+        }
+    }
+
+    let in_right_place = least_end_value_for_length
+        .into_iter()
+        .rposition(|x| x != u16::MAX)
+        .unwrap_or(n);
+
+    println!("{}", n - in_right_place);
+}
+
+struct Scanner {
+    tokens: std::collections::VecDeque<String>,
+}
+
+impl Scanner {
+    pub fn new() -> Self {
+        use std::io::BufRead;
+
+        let stdin = std::io::stdin();
+        let mut tokens = std::collections::VecDeque::new();
+        for line in stdin.lock().lines() {
+            for token in line.unwrap().split_ascii_whitespace() {
+                tokens.push_back(token.to_owned());
+            }
+        }
+        Self { tokens }
+    }
+
+    pub fn next<T: std::str::FromStr>(&mut self) -> T
+    where
+        <T as std::str::FromStr>::Err: std::fmt::Debug,
+    {
+        T::from_str(&self.tokens.pop_front().unwrap()).unwrap()
+    }
+}
+
+use std::marker::PhantomData;
+
+macro_rules! recursive_function {
+    ($name: ident, $trait: ident, ($($type: ident $arg: ident,)*)) => {
+        pub trait $trait<$($type, )*Output> {
+            fn call(&mut self, $($arg: $type,)*) -> Output;
+        }
+
+        pub struct $name<F, $($type, )*Output>
+        where
+            F: FnMut(&mut dyn $trait<$($type, )*Output>, $($type, )*) -> Output,
+        {
+            f: std::cell::UnsafeCell<F>,
+            $($arg: PhantomData<$type>,
+            )*
+            phantom_output: PhantomData<Output>,
+        }
+
+        impl<F, $($type, )*Output> $name<F, $($type, )*Output>
+        where
+            F: FnMut(&mut dyn $trait<$($type, )*Output>, $($type, )*) -> Output,
+        {
+            pub fn new(f: F) -> Self {
+                Self {
+                    f: std::cell::UnsafeCell::new(f),
+                    $($arg: Default::default(),
+                    )*
+                    phantom_output: Default::default(),
+                }
+            }
+        }
+
+        impl<F, $($type, )*Output> $trait<$($type, )*Output> for $name<F, $($type, )*Output>
+        where
+            F: FnMut(&mut dyn $trait<$($type, )*Output>, $($type, )*) -> Output,
+        {
+            fn call(&mut self, $($arg: $type,)*) -> Output {
+                unsafe { (*self.f.get())(self, $($arg, )*) }
+            }
+        }
+    }
+}
+
+recursive_function!(RecursiveFunction0, Callable0, ());
+recursive_function!(RecursiveFunction, Callable, (Arg arg,));
+recursive_function!(RecursiveFunction2, Callable2, (Arg1 arg1, Arg2 arg2,));
+recursive_function!(RecursiveFunction3, Callable3, (Arg1 arg1, Arg2 arg2, Arg3 arg3,));
+recursive_function!(RecursiveFunction4, Callable4, (Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4,));
+recursive_function!(RecursiveFunction5, Callable5, (Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4, Arg5 arg5,));
+recursive_function!(RecursiveFunction6, Callable6, (Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4, Arg5 arg5, Arg6 arg6,));
+recursive_function!(RecursiveFunction7, Callable7, (Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4, Arg5 arg5, Arg6 arg6, Arg7 arg7,));
+recursive_function!(RecursiveFunction8, Callable8, (Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4, Arg5 arg5, Arg6 arg6, Arg7 arg7, Arg8 arg8,));
+recursive_function!(RecursiveFunction9, Callable9, (Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4, Arg5 arg5, Arg6 arg6, Arg7 arg7, Arg8 arg8, Arg9 arg9,));
