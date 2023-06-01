ba122cb5e36ff3dde2795bcc69dcd11f6c820d09
Matheus Cardoso <dacachmatheus@gmail.com>
2023-05-31 23:44:32 -0300

    Add solution to 1837F
    Binary Search, Binary Heap

--- contest/1837/f/src/main.rs
+++ contest/1837/f/src/main.rs
@@ -1,4 +1,5 @@
-use std::cmp::min;
+use std::cmp::{min, Reverse};
+use std::collections::BinaryHeap;
 use std::thread::current;
 
 fn main() {
@@ -8,156 +9,79 @@ fn main() {
         let (n, k): (usize, usize) = (s.next(), s.next());
         let values: Vec<u64> = (0..n).map(|_| s.next()).collect();
 
-        // Find the k-th smallest.
-        let sorted = {
-            let mut v = values.clone();
-            v.sort();
-            v
-        };
+        let compute_maximum_length_from_left = |values: &Vec<u64>, wanted_sum: u64| {
+            let mut maximum_length_from_left = vec![0; n];
 
-        let big = sorted[k - 1];
-        let big_count = values.iter().filter(|&&x| x == big).count() as u64;
-        let big_prefix_sum: Vec<u64> = {
-            let mut res = vec![];
-            let ones = values
-                .iter()
-                .map(|&x| if x == big { 1 } else { 0 })
-                .reduce(|acc, e| {
-                    res.push(acc + e);
-                    acc + e
-                })
-                .unwrap();
-            res
-        };
-        let bigs_in_range = |l: usize, r: usize| {
-            if l == 0 {
-                big_prefix_sum[r]
-            } else {
-                big_prefix_sum[r] - big_prefix_sum[l - 1]
-            }
-        };
-
-        // These guys are part of the solution.
-        let fixed: Vec<(usize, u64)> = sorted
-            .into_iter()
-            .enumerate()
-            .take_while(|(i, x)| *x < big)
-            .collect();
-        assert!(fixed.len() <= k);
-
-        // But now we need to choose k - fixed, from big_count
-        let must_choose = (k - fixed.len()) as u64;
-
-        let mut answer = u64::MAX;
-        let mut current_prefix_sum = 0;
-        let total_sum: u64 = fixed.iter().map(|(_i, x)| x).sum();
-        for values_in_prefix in 0..fixed.len() {
-            let suffix_sum = total_sum - current_prefix_sum;
-
-            let mut bigs_available_for_prefix_only = {
-                let position_last_prefix = fixed[values_in_prefix].0;
-                bigs_in_range(0, position_last_prefix)
-            };
-
-            let mut bigs_available_for_suffix_only = {
-                let position_last_suffix = if values_in_prefix + 1 < fixed.len() {
-                    fixed[values_in_prefix + 1].0
+            let mut active = BinaryHeap::new();
+            let mut inactive = BinaryHeap::new();
+            let mut sum = 0;
+            for (i, &v) in values.iter().enumerate() {
+                if sum + v <= wanted_sum {
+                    active.push(v);
+                    sum += v;
                 } else {
-                    0
-                };
-                bigs_in_range(position_last_suffix, n - 1)
-            };
+                    inactive.push(Reverse(v));
+                    while let Some(&Reverse(best_inactive_value)) = inactive.peek() {
+                        match active.peek() {
+                            None => {
+                                assert!(sum + v > wanted_sum);
+                                break;
+                            }
+                            Some(&worst_active_value) => {
+                                if best_inactive_value < worst_active_value {
+                                    active.pop();
+                                    active.push(best_inactive_value);
+                                    sum -= worst_active_value;
 
-            let mut bigs_available_for_both =
-                { big_count - (bigs_available_for_prefix_only + bigs_available_for_suffix_only) };
-
-            if suffix_sum > current_prefix_sum {
-                // We will put `big` in prefix until prefix + big > suffix.
-                let delta = suffix_sum - current_prefix_sum;
-                let mut can_put = delta / big;
-
-                if can_put >= must_choose {
-                    let going_to_put = must_choose; // We can only put that much.
-
-                    let maximum_here = bigs_available_for_prefix_only + bigs_available_for_both;
-                    if maximum_here >= going_to_put {
-                        // We are able to put them all.
-                        // In the end, suffix is still greater, so our answer is the suffix portion anyway.
-                        // So we don't actually need to perform the operations here.
-                        answer = min(answer, suffix_sum);
-                    } else {
-                        // We can only put `maximum_here`.
-                        let remaining = going_to_put - maximum_here;
-                        // These will need to be put on the suffix portion.
-                        answer = min(answer, suffix_sum + remaining * big);
-                    }
-                } else {
-                    // We are able to put all we need on the prefix, but we still need to put more in order
-                    // to end with `k` total numbers.
-                    let going_to_put = can_put;
-                    let maximum_here = bigs_available_for_prefix_only + bigs_available_for_both;
-
-                    if maximum_here >= going_to_put {
-                        // We are able to put them all.
-                        let mut new_pref = current_prefix_sum + going_to_put * big;
-                        // Some of those came from the prefix portion and some from the both portion.
-                        if bigs_available_for_prefix_only < going_to_put {
-                            bigs_available_for_both -=
-                                going_to_put - bigs_available_for_prefix_only;
-                            bigs_available_for_prefix_only = 0;
+                                    inactive.pop();
+                                    inactive.push(Reverse(worst_active_value));
+                                    sum += best_inactive_value;
+                                } else {
+                                    break;
+                                }
+                            }
                         }
-
-                        // Afterwards, we need to start alternating between prefix and suffix until we have enough values.
-
-                        let left_needed = must_choose - going_to_put;
-                        let min_both = min(
-                            bigs_available_for_prefix_only,
-                            bigs_available_for_suffix_only,
-                        );
-                        if min_both * 2 >= left_needed {
-                            // All good, alternate.
-                            // Answer will come from suffix anyway.
-                            answer = min(answer, suffix_sum + min_both * big);
-                        } else {
-                            // One of them is 0, and then you need to pick from `both`.
-                            let new_pref = new_pref + min_both * big;
-                            let new_suf = suffix_sum + min_both * big;
-                            let left_needed = left_needed - 2 * min_both;
-                        }
-
-                        answer = min(answer, suffix_sum);
-                    } else {
-                        // We can only put `maximum_here`.
-                        let remaining = going_to_put - maximum_here;
-                        // These will need to be put on the suffix portion.
-                        answer = min(answer, suffix_sum + remaining * big);
                     }
                 }
 
-                // 1. Put `big` so that the next one would make prefix too big.
-                if bigs_available_for_prefix_only >= can_put {
-                    bigs_available_for_prefix_only -= can_put;
-                    can_put = 0;
-
-                    // And now we should alternate between prefix and suffix.
-                }
-
-                // Afterwards, we alternate putting on prefix and suffix (if possible, how?)
-            } else {
-                // We will put `big` in suffix until suffix + big > prefix.
-                let delta = current_prefix_sum - suffix_sum;
-                let can_put = delta / big;
-                // TODO: will_put is also bounded by how many are in the correct positions.
-                let will_put = min(can_put, big_count);
-
-                // Afterwards, we alternate putting on suffix and prefix (if possible, how?)
+                maximum_length_from_left[i] = active.len();
             }
 
-            // This element will be considered as part of the prefix in the next iteration.
-            current_prefix_sum += fixed[values_in_prefix].1;
+            maximum_length_from_left
+        };
+
+        let check_if_possible = |sum: u64| {
+            let maximum_length_from_left = compute_maximum_length_from_left(&values, sum);
+
+            let maximum_length_from_right: Vec<_> = {
+                let reversed = values.clone().into_iter().rev().collect();
+                compute_maximum_length_from_left(&reversed, sum)
+                    .into_iter()
+                    .rev()
+                    .collect()
+            };
+
+            for i in 0..(n - 1) {
+                if maximum_length_from_left[i] + maximum_length_from_right[i + 1] >= k {
+                    return true;
+                }
+            }
+            maximum_length_from_left[n - 1] >= k
+        };
+
+        let mut left = 0; // Never possible.
+        let mut right = 10_u64.pow(16); // Always possible.
+
+        while right - left > 1 {
+            let middle = left + (right - left) / 2;
+            if check_if_possible(middle) {
+                right = middle;
+            } else {
+                left = middle;
+            }
         }
 
-        println!("***");
+        println!("{right}");
     }
 }
 
